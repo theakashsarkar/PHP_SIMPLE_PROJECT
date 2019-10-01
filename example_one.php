50f457e4e2276980c52eaaf8d65aa9999cea2e97
theakashsarkar <theakashsarkar7@gmail.com>
2019-10-02 00:26:17 +0600

    calcultor complite

--- example_one.php
+++ example_one.php
@@ -1,6 +1,29 @@
 <?php
-  echo '<pre>';
-  print_r($_POST);
+    echo '<pre>';
+    print_r($_POST);
+
+    if(isset($_POST['btn'])){
+            function calculator($data){
+                // echo '<pre>';
+                // print_r($_POST);
+                $first_number =$data[$first_number];
+                $second_number=$data[$second_number];
+                $btn = $data['btn'];
+                if($btn == '+'){
+                    echo $first_number+$second_number;
+                }
+                else if($btn == '-'){
+                    echo $first_number-$second_number;
+                }
+                else if($btn == '*'){
+                    echo $first_number*$second_number;
+                }
+                else if($btn == '/'){
+                    echo $first_number/$second_number;
+                }
+            }
+            
+    }
 
 
 
@@ -15,15 +38,15 @@
     <table>
         <tr>
             <td>Starting Number</td>
-            <td><input type="number"  name="starting_number"></td>
+            <td><input type="number"  name="first_number"></td>
         </tr>
         <tr>
             <td>Ending Number</td>
-            <td><input type="number" name="ending_number"></td>
+            <td><input type="number" name="second_number"></td>
         </tr>
         <tr>
             <td>Result</td>
-            <td><input type="number"></td>
+            <td><input type="number" value="<?php if(isset($_POST['btn'])){calculator($_POST);} ?>"></td>
         </tr>
         <tr>
             <td></td>
